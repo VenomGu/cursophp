d1c6ba547235fd872993c87847ab9ef351c48567
Gustavo Calastro <gu.calastro@gmail.com>
2024-10-03 23:44:55 -0300

    first commit

---
+++ ex000/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOKUTO SHINKEN! </title>
+</head>
+
+<body>
+    <H1>
+        <?php 
+        
+            echo "O verdadeiro mestre de Hokuto!";
+        ?>
+    </H1>
+    <P>
+        Vamos ver que é o verdadeiro mestre de Hokuto!
+    </P>
+
+</body>
+
+</html>
